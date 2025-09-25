8c8c76c5bd2505055b8d6aa42867c632decadeb3
Gaurav Singh Baghel <gauravsbofficial18@gmail.com>
2025-09-25 10:27:29 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -32,5 +32,5 @@ app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = https://expense-tracker-8e5r.onrender.com;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
